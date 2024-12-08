0dd21c4d45bf8c9a5e9a4c90c069311031a95e8a
Mark Ryan Guamos <ryan.guamos@Marks-MacBook-Pro.local>
2024-12-09 05:56:15 +0800

    templ

--- web/view/components/navigation_item_templ.go
+++ web/view/components/navigation_item_templ.go
@@ -130,7 +130,7 @@ func NavigationItem(item MenuItem) templ.Component {
 		var templ_7745c5c3_Var5 string
 		templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(item.Label)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `web/view/components/navigation_item.templ`, Line: 49, Col: 81}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `web/view/components/navigation_item.templ`, Line: 49, Col: 80}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 		if templ_7745c5c3_Err != nil {
